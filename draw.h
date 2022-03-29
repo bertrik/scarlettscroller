262cf1872681d01d579cd1217ab0153a09631672
Bertrik Sikken <bertrik@sikken.nl>
2022-03-30 00:17:08 +0200

    Add draw_vline() to draw.h

--- draw.h
+++ draw.h
@@ -4,6 +4,7 @@
 
 void draw_init(uint8_t *framebuffer);
 bool draw_pixel(int x, int y, uint8_t p);
+void draw_vline(int x, uint8_t c);
 int draw_text(const char *text, int x, uint8_t fg, uint8_t bg);
 
 
